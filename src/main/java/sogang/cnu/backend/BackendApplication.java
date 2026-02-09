fa508e62f6d462a8285d25bed4b5cf3de9d68a0e
tjdus <dusml0277@gmail.com>
2026-02-09 19:11:32 +0900

    prepare for authorization

--- src/main/java/sogang/cnu/backend/BackendApplication.java
+++ src/main/java/sogang/cnu/backend/BackendApplication.java
@@ -3,9 +3,11 @@ package sogang.cnu.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@EnableMethodSecurity(prePostEnabled = true)
 public class BackendApplication {
 
 	public static void main(String[] args) {
